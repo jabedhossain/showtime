02348da65ec17dcd6f25f37f901f4e770e86fc30
Jabed Hossain <hossainjabed@live.com>
2019-02-14 23:59:57 -0500

    Updated the javascript file

--- popup_script.js
+++ popup_script.js
@@ -4,4 +4,12 @@ var dateId = "date";
 var days = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
 var months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
 var consoleGreeting = "Hello World! - from popup_script.js";
+function setTimeandDate(timeElement, dataElement){
+	var date = new Date();
+	var minutes = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
+	var time = date.getHours() + ":" + minutes;
+	var date = days[date.getDay()] + ", " + months[date.getMonth()] + "" + date.getDate() + "" + date.getFullYear();
+	timeElement.innerHTML = time;
+	dataElement.innerHTML = date; 
+}
 //end-region
