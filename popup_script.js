edd7287bd64812e2d1293beedeb106cf97dae8cf
Jabed Hossain <hossainjabed@live.com>
2019-02-14 23:13:08 -0500

    Add files via upload

---
+++ popup_script.js
@@ -0,0 +1,7 @@
+//region {variables and functions}
+var timeId = "time";
+var dateId = "date";
+var days = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
+var months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+var consoleGreeting = "Hello World! - from popup_script.js";
+//end-region
